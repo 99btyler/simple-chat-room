c9caa03cee724e9c56c8cb21b9a0e29c1e20e4d2
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-13 17:06:00 -0500

    Renamed handle_load() to launch()

--- src/main.py
+++ src/main.py
@@ -18,18 +18,18 @@ class Launcher():
         self.frame.grid()
 
         # Tkinter widgets
-        self.button_host = ttk.Button(self.frame, text="Host", command=lambda: self.handle_load("host"))
+        self.button_host = ttk.Button(self.frame, text="Host", command=lambda: self.launch("host"))
         self.button_host["padding"] = 50
         self.button_host.grid(column=0, row=0)
 
-        self.button_join = ttk.Button(self.frame, text="Join", command=lambda: self.handle_load("join"))
+        self.button_join = ttk.Button(self.frame, text="Join", command=lambda: self.launch("join"))
         self.button_join["padding"] = 50
         self.button_join.grid(column=0, row=1)
 
         # Tkinter mainloop
         self.root.mainloop()
     
-    def handle_load(self, type):
+    def launch(self, type):
         if type == "host":
             ScreenHost(type)
         elif type == "join":
