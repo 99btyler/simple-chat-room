613b4ace97bbabca88b7359b4d1a46395dfe6439
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-15 11:17:14 -0400

    Added widgets to Client's toplevel

--- src/chat/client.py
+++ src/chat/client.py
@@ -19,6 +19,24 @@ class Client():
         self.toplevel.resizable(False, False)
         self.toplevel.protocol("WM_DELETE_WINDOW", self.handle_close)
 
+        # toplevel widgets
+        self.label_host = tk.Label(self.toplevel, text="Host:")
+        self.label_host.grid(column=0, row=0)
+
+        self.stringvar_host = tk.StringVar()
+        self.entry_host = tk.Entry(self.toplevel, textvariable=self.stringvar_host)
+        self.entry_host.grid(column=1, row=0)
+
+        self.label_port = tk.Label(self.toplevel, text="Port:")
+        self.label_port.grid(column=0, row=1)
+
+        self.stringvar_port = tk.StringVar()
+        self.entry_port = tk.Entry(self.toplevel, textvariable=self.stringvar_port)
+        self.entry_port.grid(column=1, row=1)
+
+        self.button_connect = tk.Button(self.toplevel, text="Connect")
+        self.button_connect.grid()
+
         # socket
         if not self.socket == None:
             return
